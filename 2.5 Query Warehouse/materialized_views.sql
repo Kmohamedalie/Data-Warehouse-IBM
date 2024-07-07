1497a1bcb003764f9c3eccea512d54c116b7c1b1
Mohamed Alie Kamara <63104472+Kmohamedalie@users.noreply.github.com>
2024-07-07 17:02:08 +0200

    update

--- 2.5 Query Warehouse/materialized_views.sql
+++ 2.5 Query Warehouse/materialized_views.sql
@@ -6,3 +6,24 @@ on "FactBilling".customerid = "DimCustomer".customerid
 left join "DimMonth"
 on "FactBilling".monthid="DimMonth".monthid
 group by country,year);
+
+
+
+/*
+select year, quartername, sum(billedamount) as totalbilledamount
+from "FactBilling"
+left join "DimCustomer"
+on "FactBilling".customerid = "DimCustomer".customerid
+left join "DimMonth"
+on "FactBilling".monthid="DimMonth".monthid
+group by grouping sets(year, quartername);
+
+
+*/
+
+-- Step 2: Populate/refresh data into the Materialized views
+REFRESH MATERIALIZED VIEW countrystats;
+
+
+-- Step 3: Query the Materialized views.
+select * from countrystats;
